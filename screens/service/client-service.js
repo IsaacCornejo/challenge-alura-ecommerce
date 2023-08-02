ad8ca334cb729cd532007aa7feddc55c84ca5975
Isaac Cornejo <espinosacornejo.pr@gmail.com>
2023-08-01 20:04:32 -0600

    screen de producto agregado correctamente hecha y linkeada

--- screens/service/client-service.js
+++ screens/service/client-service.js
@@ -2,6 +2,19 @@
 const listaClientes = () =>
   fetch("http://localhost:3000/producto").then((respuesta) => respuesta.json());
 
+const id = "#1111111";
+
+const crearCliente = (imagen, categoria, titulo, precio, descripcion) => {
+  return fetch("http://localhost:3000/producto", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ imagen, categoria, titulo, precio, descripcion }),
+  });
+};
+
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
